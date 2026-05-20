52b7ed79d8aff4c2dbe294fd656e886fd4c73cf3
kawsar <kawsaruddin350@gmail.com>
2026-05-20 16:25:12 +0600

    applied pydantic schema

--- database/model.py
+++ database/model.py
@@ -6,3 +6,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from database.database import Base, engine, get_db
+from schemas import UserCreate, UserResponse
 from database.model import User
 from sqlalchemy.orm import Session
 
@@ -12,42 +13,43 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/sql/")
-def read_users(db: Session = Depends(get_db)):
+def create_tables(db: Session = Depends(get_db)):
     return {
         'message' : "db table created successfully",
     }
 
 @app.post("/user/")
-def create_user(user_name: str, db: Session = Depends(get_db)):
-    new_user = User(name=user_name)
+def create_user(user : UserCreate, db: Session = Depends(get_db)):
+    new_user = User(name=user.name, email=user.email)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 
-@app.get("/users/")
+@app.get("/users/", response_model=list[UserResponse])
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
 
-@app.get("/users/{user_id}")
+@app.get("/users/{user_id}", response_model=UserResponse)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     new_user = db.query(User).filter(User.id == user_id).first()
     if new_user:
         return new_user
     return {"message": "User not found"}
 
-@app.put("/users/{user_id}")
-def update_user(user_id: int, user_name: str, db: Session = Depends(get_db)):
+@app.put("/users/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, user : UserCreate, db: Session = Depends(get_db)):
     new_user = db.query(User).filter(User.id == user_id).first()
     if new_user:
-        new_user.name = user_name
+        new_user.name = user.name
+        new_user.email = user.email
         db.commit()
         db.refresh(new_user)
         return new_user
     return {"message": "User not found"}
 
-@app.delete("/users/{user_id}")
+@app.delete("/users/{user_id}", response_model=UserResponse)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     new_user = db.query(User).filter(User.id == user_id).first()
     if new_user:
